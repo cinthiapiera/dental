73dd289bc563b8546dfbdbb44aa33e5a8a784955
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-07-24 13:24:30 -0500

    counter

---
+++ src/components/UI/Counter.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../../styles/counter.css';
+
+const counterData=[
+  {
+    number: '3k',
+    text: 'Pacientes satisfechos',
+  },
+  {
+    number: 350,
+    text: 'Tratamientos realizados',
+  },
+  {
+    number: 900,
+    text: 'Casos de exito',
+  }
+]
+
+const Counter = () => {
+  return (
+    <section className="counter">
+      <div className="container">
+        <div className="counter__wrapper">
+          {
+            counterData.map((item,index)=>(
+              <div className="counter__item" key={index}>
+                <h3 className="counter__number">{item.number}+</h3>
+                <h4 className="counter__title">{item.text}</h4>
+              </div>
+            ))
+          }
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Counter
