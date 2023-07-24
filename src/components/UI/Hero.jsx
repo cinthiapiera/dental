733c9017b90db2ce87a56e0c7494fd591b5ccd18
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-07-24 12:00:28 -0500

    hero content

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -10,15 +10,14 @@ const Hero = () => {
         <div className="hero__wrapper">
           <div className="hero__content">
             <div>
-              <h2>Get expert help and care at every visit.</h2>
-              <h2>At Tooth Bright Dental, we value and listen to our client's specific needs.</h2>
-              <h2 className='highlight'>set an appointment</h2>
+              <h2>Obtenga ayuda y atención experta en cada visita.</h2>
+              <h2 className='highlight'>Valoramos y escuchamos las necesidades específicas de nuestros clientes.</h2>
             </div>
             <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae ratione inventore eos illum similique voluptatibus? Doloremque ipsum ad quis, deleniti sapiente cumque recusandae, placeat, maxime blanditiis fuga quisquam velit cum!</p>
           
             <div className="hero__btns">
-              <button className="primary__btn"> set an appointment </button>
-              <button className="secundary__btn">Discover More</button>
+              <button className="primary__btn"> Concertar una cita </button>
+              <button className="secondary__btn"> Descubrir más </button>
             </div>
           </div>
 
